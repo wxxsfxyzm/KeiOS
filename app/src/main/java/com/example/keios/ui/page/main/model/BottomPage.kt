d2316513121d81fb09dd8c8163da51f2bdd41615
hosizoraru <hosizoraru@gmail.com>
2026-04-10 20:18:31 +0800

    opti buttom bar icon

--- app/src/main/java/com/example/keios/ui/page/main/model/BottomPage.kt
+++ app/src/main/java/com/example/keios/ui/page/main/model/BottomPage.kt
@@ -3,18 +3,18 @@ package com.example.keios.ui.page.main.model
 import androidx.compose.ui.graphics.vector.ImageVector
 import top.yukonga.miuix.kmp.icon.MiuixIcons
 import top.yukonga.miuix.kmp.icon.extended.CloudFill
-import top.yukonga.miuix.kmp.icon.extended.Info
+import top.yukonga.miuix.kmp.icon.extended.Album
 import top.yukonga.miuix.kmp.icon.extended.Link
-import top.yukonga.miuix.kmp.icon.extended.Tasks
+import top.yukonga.miuix.kmp.icon.extended.Sidebar
 import top.yukonga.miuix.kmp.icon.extended.Tune
 
 enum class BottomPage(
     val label: String,
     val icon: ImageVector
 ) {
-    Home("主页", MiuixIcons.Regular.Tasks),
+    Home("主页", MiuixIcons.Regular.Sidebar),
     System("系统", MiuixIcons.Regular.Tune),
     Mcp("MCP", MiuixIcons.Regular.CloudFill),
     GitHub("GitHub", MiuixIcons.Regular.Link),
-    Ba("BA", MiuixIcons.Regular.Info)
+    Ba("BA", MiuixIcons.Regular.Album)
 }
