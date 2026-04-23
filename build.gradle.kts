3d87e1aa26c1ffe19cd9919c74e2c9ae6529da13
hosizoraru <hosizoraru@gmail.com>
2026-04-23 08:11:07 +0800

    fix: update Android Gradle plugin version to stable release 9.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "9.2.0-rc01" apply false
+    id("com.android.application") version "9.2.0" apply false
     id("io.github.takahirom.roborazzi") version "1.59.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.20" apply false
 }
